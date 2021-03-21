d26febe1400d06cbc12dec5259f2390eeef38866
Bryan Douglas Lima Dos Santos <bryandouglasxd@gmail.com>
2021-03-21 20:30:31 -0300

    Update Final

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const statesJSON = './databs/Estados.json';
 const citiesJSON = './databs/Cidades.json';
 
+//creation of .json files according to each state
 function readJSON() {
   const states = JSON.parse(fs.readFileSync(statesJSON, 'utf-8'));
   const cities = JSON.parse(fs.readFileSync(citiesJSON, 'utf-8'));
@@ -16,6 +17,7 @@ function readJSON() {
   });
 }
 
+//Get data from .json files
 function searchUF(UF) {
   const data = JSON.parse(fs.readFileSync(`./createDATA/${UF}.json`));
   const total = data.length;
@@ -24,7 +26,9 @@ function searchUF(UF) {
 
 /* console.log(searchUF('MG')); */
 
-function getGreaterStatesString() {
+//Get five states that least have cities
+
+function getFiveCities() {
   const states = JSON.parse(fs.readFileSync(statesJSON, 'utf-8'));
   const newArrayStates = [];
   states.forEach((state) => {
@@ -33,17 +37,46 @@ function getGreaterStatesString() {
     const objeto = { UF, quantidade_estados };
     newArrayStates.push(objeto);
   });
-  newArrayStates.sort((a, b) => a.quantidade_estados - b.quantidade_estados);
+  //Asc
+  //newArrayStates.sort((a, b) => a.quantidade_estados - b.quantidade_estados);
+  //Desc
+  newArrayStates.sort((a, b) => b.quantidade_estados - a.quantidade_estados);
   newArrayStates.length = 5;
   const arrayOfString = newArrayStates.map(
     (state) => `${state.UF} - ${state.quantidade_estados}`
   );
-  console.log(arrayOfString);
+  return arrayOfString;
 }
 
-/* getGreaterStatesString(); */
+//console.log(getFiveCities());
 
-function getNamesCities() {
+//Get biggest name of each state
+
+function getNameBigCities() {
+  const states = JSON.parse(fs.readFileSync(statesJSON, 'utf-8'));
+  const cities = JSON.parse(fs.readFileSync(citiesJSON, 'utf-8'));
+  const arrayName = [];
+  states.forEach((state) => {
+    let citiesState = cities.filter((city) => {
+      return city.Estado === state.ID;
+    });
+    let nameMax = '';
+    citiesState.forEach((city) => {
+      if (city.Nome.length > nameMax.length) nameMax = city.Nome;
+    });
+    const obj = {
+      UF: state.Sigla,
+      Cidade: nameMax,
+    };
+    arrayName.push(obj);
+    arrayName.sort((a, b) => a.Cidade.length - b.Cidade.length);
+  });
+  return arrayName;
+}
+//console.log(getNameBigCities());
+
+//Get smallest name of each state
+function getNameSmallCities() {
   const states = JSON.parse(fs.readFileSync(statesJSON, 'utf-8'));
   const cities = JSON.parse(fs.readFileSync(citiesJSON, 'utf-8'));
   const arrayName = [];
@@ -60,9 +93,10 @@ function getNamesCities() {
       Cidade: nameMax,
     };
     arrayName.push(obj);
+
+    arrayName.sort((a, b) => a.Cidade.length - b.Cidade.length);
   });
   return arrayName;
 }
-getNamesCities();
 
-console.log(getNamesCities());
+console.log(getNameSmallCities());
